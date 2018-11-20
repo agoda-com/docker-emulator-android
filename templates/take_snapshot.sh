bef1960667d7b9082f655b189d4e63fcd4a21203
Anton Malinskiy <anton@malinskiy.com>
2018-11-20 14:55:06 +0700

    Add parameterized image name with proxy support in snapshot

--- templates/take_snapshot.sh
+++ templates/take_snapshot.sh
@@ -2,13 +2,16 @@
 
 set -ex
 
+image_name=$1
+snapshot_image_name=$2
+
 docker rm -f emulator || true
 
-docker run -d -t --name emulator --rm --privileged -v /dev/kvm:/dev/kvm -e ANDROID_ARCH="x86" agoda/docker-emulator-{{ platform }} bash
+docker run -d -t --name emulator --rm --privileged -v /dev/kvm:/dev/kvm -e ANDROID_ARCH="x86" $image_name bash
 
 docker cp snapshot.sh emulator:/snapshot.sh
 docker cp snapshot.expect emulator:/snapshot.expect
 docker exec -t emulator bash -c "bash /snapshot.sh; exit"
 echo "Creating new image"
-docker commit -m "Snapshot!" --change "CMD [\"/start.sh\"]" emulator agoda/docker-emulator-snapshot-{{ platform }}
+docker commit -m "Snapshot!" --change "CMD [\"/start.sh\"]" emulator $snapshot_image_name
 docker rm -f emulator
